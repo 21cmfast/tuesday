6cfb95d20c3fcfafc6fbf66d5bf8baff4786ccf2
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2025-03-13 15:32:49 +0100

    docs: adding docs

--- src/tuesday/core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Core routines for analysing EoR simulations."""
-
-__all__ = [
-    "calculate_ps",
-    "cylindrical_to_spherical",
-]
-
-from .summaries.powerspectra import calculate_ps, cylindrical_to_spherical
--- src/tuesday/core/summaries/__init__.py
+++ src/tuesday/core/summaries/__init__.py
@@ -1,1 +1,6 @@
-"""A sub-package for calculating summaries of simulations, like power spectra."""
+"""Code to calculate the 1D and 2D power spectrum of a lightcone."""
+
+from .powerspectra import bin_kpar as bin_kpar
+from .powerspectra import calculate_ps as calculate_ps
+from .powerspectra import cylindrical_to_spherical as cylindrical_to_spherical
+from .powerspectra import postprocess_ps as postprocess_ps
