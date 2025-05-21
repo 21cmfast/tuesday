52c162b7a89ba4a0fe7d062bf9eac0e7cfc87e0d
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2025-05-21 15:45:39 +0200

    fix: plotting does one PS at a time

--- src/tuesday/core/plotting/powerspectra.py
+++ src/tuesday/core/plotting/powerspectra.py
@@ -19,12 +19,12 @@ def plot_1d_power_spectrum(  # noqa: C901
     title: str | None = None,
     xlabel: str | None = None,
     ylabel: str | None = None,
-    colors: list | None = None,
-    log: list[bool] | None = None,
+    color: list | None = None,
+    log: list[bool] | None = False,
     fontsize: float | None = 16,
-    labels: list | None = None,
+    label: list | None = None,
     smooth: float | bool = False,
-    leg_kwargs: dict | None = None,
+    legend_kwargs: dict | None = None,
 ) -> tuple[plt.Figure, plt.Axes]:
     """
     Plot a 1D power spectrum.
@@ -48,31 +48,25 @@ def plot_1d_power_spectrum(  # noqa: C901
         Label for the x-axis.
     ylabel : str, optional
         Label for the y-axis.
-    colors : list, optional
-        List of colors for each line in the plot.
+    color : str, optional
+        Color of the PS line in the plot.
     log : list[bool], optional
         List of booleans to set the x and y axes to log scale.
     fontsize : float, optional
         Font size for the plot labels.
-    labels : list, optional
-        List of labels for each line in the plot.
+    label : list, optional
+        Label for the PS.
     smooth : float, optional
         Standard deviation for Gaussian smoothing.
         If True, uses a standard deviation of 1.
-    leg_kwargs : dict, optional
+    legend_kwargs : dict, optional
         Keyword arguments for the legend.
-
     """
-    if fig is None:
-        fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-    else:
-        ax = fig.get_axes()[0]
     rcParams.update({"font.size": fontsize})
-    if power_spectrum.ndim == 1:
-        power_spectrum = np.expand_dims(power_spectrum, axis=0)
-    n = power_spectrum.shape[0]
-    if colors is None:
-        colors = [f"C{i}" for i in range(n)]
+    if power_spectrum.ndim > 1:
+        raise ValueError("Plot one 1D PS at a time.")
+    if color is None:
+        color = "C0"
     if xlabel is None:
         if wavemodes.unit == 1 / un.Mpc:
             xlabel = r"$k \, [\rm{Mpc}^{-1}]$"
@@ -104,15 +98,7 @@ def plot_1d_power_spectrum(  # noqa: C901
         if isinstance(smooth, bool):
             smooth = 1.0
         power_spectrum = gaussian_filter(power_spectrum, sigma=smooth)
-    for i in range(n):
-        if labels is not None:
-            if len(labels) >= n:
-                label = labels[i]
-            elif len(labels) == 1 and i == 0:
-                label = labels[0]
-        else:
-            label = None
-        ax.plot(wavemodes, power_spectrum[i], color=colors[i], label=label)
+    ax.plot(wavemodes, power_spectrum, color=color, label=label)
     if title is not None:
         ax.set_title(title, fontsize=fontsize)
     ax.set_xlabel(xlabel, fontsize=fontsize)
@@ -121,8 +107,8 @@ def plot_1d_power_spectrum(  # noqa: C901
         ax.set_xscale("log")
     if log[1]:
         ax.set_yscale("log")
-    if labels is not None:
-        ax.legend(**leg_kwargs)
+    if label is not None:
+        ax.legend(**legend_kwargs)
     return fig, ax
 
 
@@ -130,8 +116,8 @@ def plot_2d_power_spectrum(  # noqa: C901
     wavemodes: un.Quantity,
     power_spectrum: un.Quantity,
     fig: plt.Figure | None = None,
-    axs: plt.Axes | list[plt.Axes] | None = None,
-    title: list[str] | str | None = None,
+    ax: plt.Axes | None = None,
+    title: str | None = None,
     xlabel: str | None = None,
     ylabel: str | None = None,
     clabel: str | None = None,
@@ -139,8 +125,9 @@ def plot_2d_power_spectrum(  # noqa: C901
     fontsize: float | None = 16,
     vmin: float | None = None,
     vmax: float | None = None,
-    log: list[bool] | None = None,
+    log: list[bool] | None = False,
     smooth: float | bool = False,
+    cbar: bool | None = True,
 ) -> tuple[plt.Figure, plt.Axes]:
     """
     Plot a 2D power spectrum.
@@ -181,23 +168,6 @@ def plot_2d_power_spectrum(  # noqa: C901
         Standard deviation for Gaussian smoothing.
         If True, uses a standard deviation of 1.
     """
-    if power_spectrum.ndim == 2:
-        power_spectrum = np.expand_dims(power_spectrum, axis=0)
-    n = power_spectrum.shape[0]
-    if fig is None:
-        fig, axs = plt.subplots(
-            nrows=1, ncols=n, figsize=(7 * n, 6), sharey=True, sharex=True
-        )
-        if n == 1:
-            axs = [axs]
-    if axs is None and fig is not None:
-        axs = fig.get_axes()
-        if isinstance(axs, plt.Axes):
-            axs = [axs]
-        if len(axs) != n:
-            raise ValueError(
-                "Number of axes on fig does not match number of power spectra!"
-            )
     rcParams.update({"font.size": fontsize})
     kperp = wavemodes[0]
     kpar = wavemodes[1]
@@ -249,9 +219,9 @@ def plot_2d_power_spectrum(  # noqa: C901
         cmap_kwargs = {}
         cmap_kwargs["norm"] = LogNorm(vmin=vmin, vmax=vmax)
 
-    if title is not None and isinstance(title, str):
-        axs[0].set_title(title, fontsize=fontsize)
-    axs[0].set_ylabel(ylabel, fontsize=fontsize)
+    if title is not None:
+        ax.set_title(title, fontsize=fontsize)
+    ax.set_ylabel(ylabel, fontsize=fontsize)
     if (isinstance(smooth, bool) and smooth) or (
         isinstance(smooth, float) and smooth > 0
     ):
@@ -259,25 +229,24 @@ def plot_2d_power_spectrum(  # noqa: C901
             smooth = 1.0
             unit = power_spectrum.unit
         power_spectrum = gaussian_filter(power_spectrum, sigma=smooth) * unit
-    for i in range(n):
-        im = axs[i].pcolormesh(
-            kperp.value,
-            kpar.value,
-            power_spectrum[i].value.T,
-            cmap=cmap,
-            **cmap_kwargs,
-        )
+    
+    im = ax.pcolormesh(
+        kperp.value,
+        kpar.value,
+        power_spectrum.value.T,
+        cmap=cmap,
+        **cmap_kwargs,
+    )
 
-        if title is not None and isinstance(title, list):
-            axs[i].set_title(title[i], fontsize=fontsize)
-        axs[i].set_xlabel(xlabel, fontsize=fontsize)
-    plt.colorbar(im, label=clabel)
+    ax.set_xlabel(xlabel, fontsize=fontsize)
+    if cbar:
+        plt.colorbar(im, label=clabel)
     if log[0]:
-        axs[0].set_xscale("log")
+        ax.set_xscale("log")
     if log[1]:
-        axs[0].set_yscale("log")
+        ax.set_yscale("log")
 
-    return fig, axs
+    return fig, ax
 
 
 def plot_power_spectrum(
@@ -290,14 +259,14 @@ def plot_power_spectrum(
     ylabel: str | None = None,
     clabel: str | None = None,
     cmap: str | None = "viridis",
-    colors: list | None = None,
+    color: list | None = None,
     fontsize: float | None = 16,
     vmin: float | None = None,
     vmax: float | None = None,
-    log: list[bool] | None = None,
-    labels: list | None = None,
+    log: list[bool] | None = False,
+    label: list | None = None,
     smooth: float | bool = False,
-    leg_kwargs: dict | None = None,
+    legend_kwargs: dict | None = None,
 ) -> tuple[plt.Figure, plt.Axes]:
     """
     Plot the power spectrum.
@@ -337,34 +306,39 @@ def plot_power_spectrum(
     smooth : bool or float, optional
         Standard deviation for Gaussian smoothing.
         If True, uses a standard deviation of 1.
-    leg_kwargs : dict, optional
+    legend_kwargs : dict, optional
         Keyword arguments for the legend.
     """
-    if isinstance(labels, str):
-        labels = [labels]
+    if isinstance(label, str):
+        label = [label]
     if not np.iterable(log):
         log = [log]
     if (hasattr(wavemodes, "ndim") and wavemodes.ndim == 1) or len(wavemodes) == 1:
-        if leg_kwargs is None:
-            leg_kwargs = {}
+        if legend_kwargs is None:
+            legend_kwargs = {}
         if len(log) == 1:
             log = [log[0], log[0]]
+        if fig is None:
+            fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+        if ax is None:
+            ax = fig.get_axes()[0]
         fig, ax = plot_1d_power_spectrum(
             wavemodes,
             power_spectrum,
             fig=fig,
+            ax=ax,
             title=title,
             xlabel=xlabel,
             ylabel=ylabel,
-            colors=colors,
+            colors=color,
             fontsize=fontsize,
             log=log,
-            labels=labels,
+            label=label,
             smooth=smooth,
-            leg_kwargs=leg_kwargs,
+            legend_kwargs=legend_kwargs,
         )
     elif (hasattr(wavemodes, "ndim") and wavemodes.ndim == 2) or len(wavemodes) == 2:
-        if labels is not None or leg_kwargs is not None:
+        if label is not None or legend_kwargs is not None:
             warnings.warn(
                 "Cylindrical PS plots do not support labels and legends.", stacklevel=2
             )
@@ -372,10 +346,30 @@ def plot_power_spectrum(
             log = [log[0], log[0], True]
         if len(log) == 2:
             log = [log[0], log[1], True]
+        if power_spectrum.ndim > 2:
+            raise ValueError("Plot one 2D PS at a time.")
+        if fig is None:
+            if ax is None:
+                fig, ax = plt.subplots(
+                    nrows=1, ncols=1, figsize=(7, 6), sharey=True, sharex=True
+                )
+                cbar = True
+            else:
+                fig = ax.get_figure()
+                if len(fig.get_axes()) > 1:
+                    cbar = False
+
+        if ax is None:
+            ax = fig.get_axes()[0]
+            if len(fig.get_axes()) > 1:
+                cbar = False
+            else:
+                cbar = True
         fig, ax = plot_2d_power_spectrum(
             wavemodes,
             power_spectrum,
             fig=fig,
+            ax=ax,
             title=title,
             xlabel=xlabel,
             ylabel=ylabel,
@@ -386,6 +380,7 @@ def plot_power_spectrum(
             vmax=vmax,
             log=log,
             smooth=smooth,
+            cbar=cbar,
         )
     else:
         raise ValueError("Wavemodes must be 1D or 2D arrays.")
