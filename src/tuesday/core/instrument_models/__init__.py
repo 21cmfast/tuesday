4fa1b9848c0b2a838c783aae5033bc3e22a17ee3
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2025-10-20 04:42:35 +0200

    docs: fix imports for rtd autosummary

--- src/tuesday/core/instrument_models/__init__.py
+++ src/tuesday/core/instrument_models/__init__.py
@@ -1,16 +1,1 @@
 """A sub-package for adding instrumental effects to simulations."""
-
-__all__ = [
-    "apply_wedge_filter",
-    "compute_thermal_rms_per_snapshot_vis",
-    "compute_thermal_rms_uvgrid",
-    "observe_lightcone",
-    "sample_from_rms_uvgrid",
-]
-from .noise import (
-    apply_wedge_filter,
-    compute_thermal_rms_per_snapshot_vis,
-    compute_thermal_rms_uvgrid,
-    observe_lightcone,
-    sample_from_rms_uvgrid,
-)
